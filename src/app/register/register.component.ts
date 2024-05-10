f0239c18dcd1f095c6f6fae79315940b7a6726f4
Carolin Emad <88932658+carolemad965@users.noreply.github.com>
2024-05-11 01:56:24 +0300

    Add validation to registration form inputs

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {RegisterService} from '../services/account/register.service'
+import {RegisterService} from '../services/account/register.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms'; // Import NgForm
 
 
 @Component({
@@ -12,23 +14,27 @@ import {RegisterService} from '../services/account/register.service'
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  userData: any = {
+    userData: any = {
     userName: '',
     password: '',
     email: '',
     accountType:'User'
   };
-  constructor(private registerService:RegisterService){
+  errorMessage: boolean =false;
+  constructor(private registerService:RegisterService,private router: Router){
 
   }
-  onSubmit(){
+  onSubmit(form: NgForm) {
     this.registerService.postData(this.userData).subscribe({
-      next:((response:any)=>{
-     console.log(response)
-      }),
-      error:((err:any)=>{
-console.log(err)
-      })
-    })
+      next: (response: any) => {
+        this.router.navigateByUrl('/login');
+      },
+      error: (err: any) => {
+        if (err.status === 400) {
+          this.errorMessage = true;
+          form.reset(); 
+        }
+      }
+    });
   }
 }
