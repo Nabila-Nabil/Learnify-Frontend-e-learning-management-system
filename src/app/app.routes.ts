01b217056e392e76102aae588b2b3ab0a0382177
Maria <maria.ezat2001@gmail.com>
2024-05-11 00:47:19 +0300

    courses Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
+    {path:'courses',component:CoursesComponent}
 ];
